2fd97d54c9ab6ae094fe7ef3e874690fc38419fe
Mateus Franceschina <mateusfranceschina@gmail.com>
2022-09-16 21:24:37 -0600

    add logs

--- api/cipher.py
+++ api/cipher.py
@@ -15,5 +15,7 @@ class handler(BaseHTTPRequestHandler):
       content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
       post_data = self.rfile.read(content_length) # <--- Gets the data itself
       print("POST request,\nPath: {}\nHeaders:\n{}\n\nBody:\n{}\n".format(str(self.path), str(self.headers), post_data.decode('utf-8')))
-
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
       self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))

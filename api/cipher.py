dd4a17005847435529a7652f5b14c5c687fcfd40
Mateus Franceschina <mateusfranceschina@gmail.com>
2022-09-16 20:51:04 -0600

    add logs

--- api/cipher.py
+++ api/cipher.py
@@ -4,8 +4,6 @@ from datetime import datetime
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-    post_data = self.rfile.read(content_length) # <--- Gets the data itself
     print(self.headers)
     print(post_data)
     self.send_response(200)

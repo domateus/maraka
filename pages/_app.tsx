395d912e6c81cdd5f7ea1ab71460779dc113fcde
Mateus Franceschina <mateusfranceschina@gmail.com>
2022-11-22 23:22:18 -0700

    deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { RootState, store, wrapper } from "@/context/store";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Provider, useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "styles/theme";
@@ -9,6 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { theme } = useSelector((state: RootState) => state.session);
   return (
     <>
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <Provider store={store}>
         <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
           <Component {...pageProps} />

d1bbef0a4e90de0ef4e69716af7ec87832038d88
Mateus Franceschina <mateusfranceschina@gmail.com>
2022-12-01 11:42:00 -0700

    dh

--- src/cipher/utils.ts
+++ src/cipher/utils.ts
@@ -1,4 +1,5 @@
 import { BigInteger } from "jsbn";
+import forge from "node-forge";
 
 export const LIST_PRIMES = [7n, 11n, 13n, 17n, 19n, 23n, 29n, 31n, 37n, 41n, 43n, 47n, 53n, 59n, 61n, 67n, 71n, 73n, 79n, 83n, 89n, 97n,
   101n, 103n, 107n, 109n, 113n, 127n, 131n, 137n, 139n, 149n, 151n, 157n, 163n, 167n, 173n, 179n, 181n, 191n, 193n, 197n, 199n,
@@ -24,6 +25,19 @@ export function generateRandomString(length: number): string {
   const result = getRandomBigInts().join("");
   console.log("OTP key", result);
   return result;
+}
+
+export async function generatePrime(bytes: number): Promise<BigInteger> {
+  return await new Promise((resolve, reject) =>{
+    (forge as any).prime.generateProbablePrime(bytes * 8, function(e: any, p: bigint) {
+      if (e) {
+        reject(e);
+      } else {
+        const v = new BigInteger(p.toString(16), 16)
+        
+      resolve(v);
+    }});
+  })
 }
 
 export function gcd(a: bigint, b: bigint): bigint {
